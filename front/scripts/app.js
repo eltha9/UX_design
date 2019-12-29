78a0ebd64b7f09623314deb29a162118b9d4f2f0
eltha9 <theadosto@gmail.com>
2019-12-29 18:45:53 +0100

    add and finish left side upper button

---
+++ front/scripts/app.js
@@ -0,0 +1,22 @@
+const left_div = document.querySelector('.left-div')
+
+
+// upper button
+let upper_buttons = {
+    left: left_div.querySelector('.upper-menu .upper-left-button'),
+    right: left_div.querySelector('.upper-menu .upper-right-button'),
+    notif: left_div.querySelector('.upper-menu .upper-right-button svg circle')
+}
+
+upper_buttons.left.addEventListener('click', ()=>{
+    if( !upper_buttons.left.classList.contains('selected')){
+        upper_buttons.left.classList.toggle('selected')
+        upper_buttons.right.classList.toggle('selected')
+    }
+})
+upper_buttons.right.addEventListener('click', ()=>{
+    if( !upper_buttons.right.classList.contains('selected')){
+        upper_buttons.right.classList.toggle('selected')
+        upper_buttons.left.classList.toggle('selected')
+    }
+})
